7f747c0beb124a4c244ead58cbd6a00143848c79
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-06-20 12:41:34 +0900

    Update fillet.

--- preAlpha/webworker.js
+++ preAlpha/webworker.js
@@ -16826,198 +16826,6 @@ return d[d.length-1];};return ", funcName].join("");
 
   /* global Worker */
 
-  var require$$0 = {};
-
-  var fs = /*#__PURE__*/Object.freeze({
-    'default': require$$0
-  });
-
-  let base = '';
-
-  // Copyright Joyent, Inc. and other Node contributors.
-
-  // Split a filename into [root, dir, basename, ext], unix version
-  // 'root' is just a slash, or nothing.
-  var splitPathRe =
-      /^(\/?|)([\s\S]*?)((?:\.{1,2}|[^\/]+?|)(\.[^.\/]*|))(?:[\/]*)$/;
-  var splitPath = function(filename) {
-    return splitPathRe.exec(filename).slice(1);
-  };
-
-  function dirname(path) {
-    var result = splitPath(path),
-        root = result[0],
-        dir = result[1];
-
-    if (!root && !dir) {
-      // No dirname whatsoever
-      return '.';
-    }
-
-    if (dir) {
-      // It has a dirname, strip trailing slash
-      dir = dir.substr(0, dir.length - 1);
-    }
-
-    return root + dir;
-  }
-
-  var toByteArray_1 = toByteArray$1;
-  var fromByteArray_1 = fromByteArray$1;
-
-  var lookup$1 = [];
-  var revLookup$1 = [];
-  var Arr$1 = typeof Uint8Array !== 'undefined' ? Uint8Array : Array;
-
-  var code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-  for (var i = 0, len = code.length; i < len; ++i) {
-    lookup$1[i] = code[i];
-    revLookup$1[code.charCodeAt(i)] = i;
-  }
-
-  // Support decoding URL-safe base64 strings, as Node.js does.
-  // See: https://en.wikipedia.org/wiki/Base64#URL_applications
-  revLookup$1['-'.charCodeAt(0)] = 62;
-  revLookup$1['_'.charCodeAt(0)] = 63;
-
-  function getLens (b64) {
-    var len = b64.length;
-
-    if (len % 4 > 0) {
-      throw new Error('Invalid string. Length must be a multiple of 4')
-    }
-
-    // Trim off extra bytes after placeholder bytes are found
-    // See: https://github.com/beatgammit/base64-js/issues/42
-    var validLen = b64.indexOf('=');
-    if (validLen === -1) validLen = len;
-
-    var placeHoldersLen = validLen === len
-      ? 0
-      : 4 - (validLen % 4);
-
-    return [validLen, placeHoldersLen]
-  }
-
-  function _byteLength (b64, validLen, placeHoldersLen) {
-    return ((validLen + placeHoldersLen) * 3 / 4) - placeHoldersLen
-  }
-
-  function toByteArray$1 (b64) {
-    var tmp;
-    var lens = getLens(b64);
-    var validLen = lens[0];
-    var placeHoldersLen = lens[1];
-
-    var arr = new Arr$1(_byteLength(b64, validLen, placeHoldersLen));
-
-    var curByte = 0;
-
-    // if there are placeholders, only get up to the last complete 4 chars
-    var len = placeHoldersLen > 0
-      ? validLen - 4
-      : validLen;
-
-    for (var i = 0; i < len; i += 4) {
-      tmp =
-        (revLookup$1[b64.charCodeAt(i)] << 18) |
-        (revLookup$1[b64.charCodeAt(i + 1)] << 12) |
-        (revLookup$1[b64.charCodeAt(i + 2)] << 6) |
-        revLookup$1[b64.charCodeAt(i + 3)];
-      arr[curByte++] = (tmp >> 16) & 0xFF;
-      arr[curByte++] = (tmp >> 8) & 0xFF;
-      arr[curByte++] = tmp & 0xFF;
-    }
-
-    if (placeHoldersLen === 2) {
-      tmp =
-        (revLookup$1[b64.charCodeAt(i)] << 2) |
-        (revLookup$1[b64.charCodeAt(i + 1)] >> 4);
-      arr[curByte++] = tmp & 0xFF;
-    }
-
-    if (placeHoldersLen === 1) {
-      tmp =
-        (revLookup$1[b64.charCodeAt(i)] << 10) |
-        (revLookup$1[b64.charCodeAt(i + 1)] << 4) |
-        (revLookup$1[b64.charCodeAt(i + 2)] >> 2);
-      arr[curByte++] = (tmp >> 8) & 0xFF;
-      arr[curByte++] = tmp & 0xFF;
-    }
-
-    return arr
-  }
-
-  function tripletToBase64$1 (num) {
-    return lookup$1[num >> 18 & 0x3F] +
-      lookup$1[num >> 12 & 0x3F] +
-      lookup$1[num >> 6 & 0x3F] +
-      lookup$1[num & 0x3F]
-  }
-
-  function encodeChunk$1 (uint8, start, end) {
-    var tmp;
-    var output = [];
-    for (var i = start; i < end; i += 3) {
-      tmp =
-        ((uint8[i] << 16) & 0xFF0000) +
-        ((uint8[i + 1] << 8) & 0xFF00) +
-        (uint8[i + 2] & 0xFF);
-      output.push(tripletToBase64$1(tmp));
-    }
-    return output.join('')
-  }
-
-  function fromByteArray$1 (uint8) {
-    var tmp;
-    var len = uint8.length;
-    var extraBytes = len % 3; // if we have 1 byte left, pad 2 bytes
-    var parts = [];
-    var maxChunkLength = 16383; // must be multiple of 3
-
-    // go through the array every three bytes, we'll deal with trailing stuff later
-    for (var i = 0, len2 = len - extraBytes; i < len2; i += maxChunkLength) {
-      parts.push(encodeChunk$1(
-        uint8, i, (i + maxChunkLength) > len2 ? len2 : (i + maxChunkLength)
-      ));
-    }
-
-    // pad the end with zeros, but make sure to not forget the extra bytes
-    if (extraBytes === 1) {
-      tmp = uint8[len - 1];
-      parts.push(
-        lookup$1[tmp >> 2] +
-        lookup$1[(tmp << 4) & 0x3F] +
-        '=='
-      );
-    } else if (extraBytes === 2) {
-      tmp = (uint8[len - 2] << 8) + uint8[len - 1];
-      parts.push(
-        lookup$1[tmp >> 10] +
-        lookup$1[(tmp >> 4) & 0x3F] +
-        lookup$1[(tmp << 2) & 0x3F] +
-        '='
-      );
-    }
-
-    return parts.join('')
-  }
-
-  const files = {};
-  const fileCreationWatchers = [];
-
-  const getFile = (options, path) => {
-    let file = files[path];
-    if (file === undefined) {
-      file = { path: path, watchers: [] };
-      files[path] = file;
-      for (const watcher of fileCreationWatchers) {
-        watcher(options, file);
-      }
-    }
-    return file;
-  };
-
   var localforage = createCommonjsModule(function (module, exports) {
   /*!
       localForage -- Offline Storage, Improved
@@ -19813,6 +19621,198 @@ return d[d.length-1];};return ", funcName].join("");
   },{"3":3}]},{},[4])(4)
   });
   });
+
+  var require$$0 = {};
+
+  var fs = /*#__PURE__*/Object.freeze({
+    'default': require$$0
+  });
+
+  let base = '';
+
+  // Copyright Joyent, Inc. and other Node contributors.
+
+  // Split a filename into [root, dir, basename, ext], unix version
+  // 'root' is just a slash, or nothing.
+  var splitPathRe =
+      /^(\/?|)([\s\S]*?)((?:\.{1,2}|[^\/]+?|)(\.[^.\/]*|))(?:[\/]*)$/;
+  var splitPath = function(filename) {
+    return splitPathRe.exec(filename).slice(1);
+  };
+
+  function dirname(path) {
+    var result = splitPath(path),
+        root = result[0],
+        dir = result[1];
+
+    if (!root && !dir) {
+      // No dirname whatsoever
+      return '.';
+    }
+
+    if (dir) {
+      // It has a dirname, strip trailing slash
+      dir = dir.substr(0, dir.length - 1);
+    }
+
+    return root + dir;
+  }
+
+  var toByteArray_1 = toByteArray$1;
+  var fromByteArray_1 = fromByteArray$1;
+
+  var lookup$1 = [];
+  var revLookup$1 = [];
+  var Arr$1 = typeof Uint8Array !== 'undefined' ? Uint8Array : Array;
+
+  var code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+  for (var i = 0, len = code.length; i < len; ++i) {
+    lookup$1[i] = code[i];
+    revLookup$1[code.charCodeAt(i)] = i;
+  }
+
+  // Support decoding URL-safe base64 strings, as Node.js does.
+  // See: https://en.wikipedia.org/wiki/Base64#URL_applications
+  revLookup$1['-'.charCodeAt(0)] = 62;
+  revLookup$1['_'.charCodeAt(0)] = 63;
+
+  function getLens (b64) {
+    var len = b64.length;
+
+    if (len % 4 > 0) {
+      throw new Error('Invalid string. Length must be a multiple of 4')
+    }
+
+    // Trim off extra bytes after placeholder bytes are found
+    // See: https://github.com/beatgammit/base64-js/issues/42
+    var validLen = b64.indexOf('=');
+    if (validLen === -1) validLen = len;
+
+    var placeHoldersLen = validLen === len
+      ? 0
+      : 4 - (validLen % 4);
+
+    return [validLen, placeHoldersLen]
+  }
+
+  function _byteLength (b64, validLen, placeHoldersLen) {
+    return ((validLen + placeHoldersLen) * 3 / 4) - placeHoldersLen
+  }
+
+  function toByteArray$1 (b64) {
+    var tmp;
+    var lens = getLens(b64);
+    var validLen = lens[0];
+    var placeHoldersLen = lens[1];
+
+    var arr = new Arr$1(_byteLength(b64, validLen, placeHoldersLen));
+
+    var curByte = 0;
+
+    // if there are placeholders, only get up to the last complete 4 chars
+    var len = placeHoldersLen > 0
+      ? validLen - 4
+      : validLen;
+
+    for (var i = 0; i < len; i += 4) {
+      tmp =
+        (revLookup$1[b64.charCodeAt(i)] << 18) |
+        (revLookup$1[b64.charCodeAt(i + 1)] << 12) |
+        (revLookup$1[b64.charCodeAt(i + 2)] << 6) |
+        revLookup$1[b64.charCodeAt(i + 3)];
+      arr[curByte++] = (tmp >> 16) & 0xFF;
+      arr[curByte++] = (tmp >> 8) & 0xFF;
+      arr[curByte++] = tmp & 0xFF;
+    }
+
+    if (placeHoldersLen === 2) {
+      tmp =
+        (revLookup$1[b64.charCodeAt(i)] << 2) |
+        (revLookup$1[b64.charCodeAt(i + 1)] >> 4);
+      arr[curByte++] = tmp & 0xFF;
+    }
+
+    if (placeHoldersLen === 1) {
+      tmp =
+        (revLookup$1[b64.charCodeAt(i)] << 10) |
+        (revLookup$1[b64.charCodeAt(i + 1)] << 4) |
+        (revLookup$1[b64.charCodeAt(i + 2)] >> 2);
+      arr[curByte++] = (tmp >> 8) & 0xFF;
+      arr[curByte++] = tmp & 0xFF;
+    }
+
+    return arr
+  }
+
+  function tripletToBase64$1 (num) {
+    return lookup$1[num >> 18 & 0x3F] +
+      lookup$1[num >> 12 & 0x3F] +
+      lookup$1[num >> 6 & 0x3F] +
+      lookup$1[num & 0x3F]
+  }
+
+  function encodeChunk$1 (uint8, start, end) {
+    var tmp;
+    var output = [];
+    for (var i = start; i < end; i += 3) {
+      tmp =
+        ((uint8[i] << 16) & 0xFF0000) +
+        ((uint8[i + 1] << 8) & 0xFF00) +
+        (uint8[i + 2] & 0xFF);
+      output.push(tripletToBase64$1(tmp));
+    }
+    return output.join('')
+  }
+
+  function fromByteArray$1 (uint8) {
+    var tmp;
+    var len = uint8.length;
+    var extraBytes = len % 3; // if we have 1 byte left, pad 2 bytes
+    var parts = [];
+    var maxChunkLength = 16383; // must be multiple of 3
+
+    // go through the array every three bytes, we'll deal with trailing stuff later
+    for (var i = 0, len2 = len - extraBytes; i < len2; i += maxChunkLength) {
+      parts.push(encodeChunk$1(
+        uint8, i, (i + maxChunkLength) > len2 ? len2 : (i + maxChunkLength)
+      ));
+    }
+
+    // pad the end with zeros, but make sure to not forget the extra bytes
+    if (extraBytes === 1) {
+      tmp = uint8[len - 1];
+      parts.push(
+        lookup$1[tmp >> 2] +
+        lookup$1[(tmp << 4) & 0x3F] +
+        '=='
+      );
+    } else if (extraBytes === 2) {
+      tmp = (uint8[len - 2] << 8) + uint8[len - 1];
+      parts.push(
+        lookup$1[tmp >> 10] +
+        lookup$1[(tmp >> 4) & 0x3F] +
+        lookup$1[(tmp << 2) & 0x3F] +
+        '='
+      );
+    }
+
+    return parts.join('')
+  }
+
+  const files = {};
+  const fileCreationWatchers = [];
+
+  const getFile = (options, path) => {
+    let file = files[path];
+    if (file === undefined) {
+      file = { path: path, watchers: [] };
+      files[path] = file;
+      for (const watcher of fileCreationWatchers) {
+        watcher(options, file);
+      }
+    }
+    return file;
+  };
 
   /* global self */
 
