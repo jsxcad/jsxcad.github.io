07c1afb68a78cb503fba86ef421b94c3677972e3
Brian Spilsbury <brian.spilsbury@gmail.com>
2019-06-28 23:48:58 +0900

    Update

--- preAlpha/master.js
+++ preAlpha/master.js
@@ -68028,47 +68028,6 @@ return d[d.length-1];};return ", funcName].join("");
 
   // Relax the coplanar arrangement into polygon soup.
 
-  // Rewrite this to handle disjointAssembly items.
-
-  /*
-  // Traverse the assembly tree and disjoint it backward.
-  export const toDisjointGeometry = (untransformedGeometry) => {
-    const geometry = toTransformedGeometry(untransformedGeometry);
-
-    if (geometry.disjointGeometry === undefined) {
-      if (geometry.assembly === undefined) {
-        // A singleton is disjoint.
-        geometry.disjointGeometry = geometry;
-      } else {
-        const subtractions = [];
-        const walk = (geometry, disjointed) => {
-          for (let nth = geometry.assembly.length - 1; nth >= 0; nth--) {
-            const item = geometry.assembly[nth];
-            if (item.assembly !== undefined) {
-              disjointed.assembly.push(walk(item, { assembly: [], tags: item.tags }));
-            } else {
-              if (item.tags === undefined || !item.tags.includes('@drop')) {
-                // Undropped items need to be trimmed.
-                const differenced = differenceItems(item, ...subtractions);
-                disjointed.assembly.push(differenced);
-                subtractions.push(differenced);
-              } else {
-                // Dropped items do not need to be trimmed.
-                disjointed.assembly.push(item);
-                subtractions.push(item);
-              }
-            }
-          }
-          return disjointed;
-        };
-        const result = walk(geometry, { assembly: [], tags: geometry.tags });
-        geometry.disjointGeometry = result;
-      }
-    }
-    return geometry.disjointGeometry;
-  };
-  */
-
   // Produce a standard geometry representation without caches, etc.
 
   const pointsToThreejsPoints = (geometry) => {
