652a9996d12c5f5eeef2aa5f7309c6d89f3f8a8e
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-01-14 20:46:24 +0900

    Automatic versioning

--- preAlphaHead/master.js
+++ preAlphaHead/master.js
@@ -3,6 +3,7 @@
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
     const bootstrap = async (sha) => {
+      document.jsxcadVersion = sha;
       const installUi = await import(`https://gitcdn.link/cdn/jsxcad/JSxCAD/${sha}/es6/jsxcad-ui-v1.js`);
       const hash = location.hash.substring(1);
       const [project, source] = hash.split('@');
