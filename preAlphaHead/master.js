c86f2cf21f99351c2cccfb05404b24af1bfb3901
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-01-14 09:38:23 +0900

    Update version manually

--- preAlphaHead/master.js
+++ preAlphaHead/master.js
@@ -1,4 +1,4 @@
-import { installUi } from 'https://gitcdn.link/cdn/jsxcad/JSxCAD/master/es6/jsxcad-ui-v1.js';
+import { installUi } from 'https://gitcdn.link/cdn/jsxcad/JSxCAD/4b8af057238e95781e3d9796624a3f466a1dd01e/es6/jsxcad-ui-v1.js';
 
 /* global location */
 
