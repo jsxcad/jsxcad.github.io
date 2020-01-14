6ef5687a166edaaefb0bf528eb00e41a3466c34f
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-01-14 09:45:45 +0900

    Update version automatically

--- preAlphaHead/master.js
+++ preAlphaHead/master.js
@@ -1,10 +1,9 @@
-import { installUi } from 'https://gitcdn.link/cdn/jsxcad/JSxCAD/4b8af057238e95781e3d9796624a3f466a1dd01e/es6/jsxcad-ui-v1.js';
-
-/* global location */
+/* global location, window */
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    const bootstrap = async () => {
+    const bootstrap = async (sha) => {
+      const installUi = await import(`https://gitcdn.link/cdn/jsxcad/JSxCAD/${sha}/es6/jsxcad-ui-v1.js`);
       const hash = location.hash.substring(1);
       const [project, source] = hash.split('@');
       await installUi({
@@ -14,6 +13,8 @@ document.onreadystatechange = () => {
       });
     };
 
-    bootstrap();
+    window.fetch('https://api.github.com/repos/jsxcad/JSxCAD/commits?per_page=1')
+          .then(response => response.json())
+          .then(json => bootstrap(json[0].sha));
   }
 };
