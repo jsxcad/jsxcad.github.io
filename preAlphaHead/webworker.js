f4a89c3f2e26fab13e32f5a051106c004b07df50
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-09-05 00:29:54 +0900

    Add auxilliary boot queue

--- preAlphaHead/webworker.js
+++ preAlphaHead/webworker.js
@@ -1,3 +1,8 @@
 const sha = self.location.hash.substring(1);
 
+// We need to start receiving messages immediately, but we're not ready to process them yet.
+// Put them in a buffer.
+self.messageBootQueue = [];
+onmessage = ({ data }) => self.messageBootQueue.push(data);
+
 import(`https://gitcdn.link/cdn/jsxcad/JSxCAD/${sha}/es6/jsxcad-ui-v1-webworker.js`);
