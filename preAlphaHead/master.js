a0324a31bf80c39ca5f4d4bdbf07899965dd01a8
Brian Spilsbury <brian.spilsbury@gmail.com>
2020-09-14 19:32:08 +0900

    Update boot message.

--- preAlphaHead/master.js
+++ preAlphaHead/master.js
@@ -3,7 +3,9 @@
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
     const bootstrap = async (sha) => {
-      document.getElementById('loading').innerText = `Booting ${sha}`;
+      const booting = document.createElement('center');
+      booting.innerText = sha;
+      document.getElementById('loading').appendChild(booting);
       const module = await import(`https://gitcdn.link/cdn/jsxcad/JSxCAD/${sha}/es6/jsxcad-ui-v1.js`);
       const hash = location.hash.substring(1);
       const [project, source] = hash.split('@');
