b2af3719f6392ac3dbfb8343f5603ac519b49cda
Brian Spilsbury <brian.spilsbury@gmail.com>
2021-08-24 14:11:47 +0900

    Repair master.js

--- preAlphaHead/master.js
+++ preAlphaHead/master.js
@@ -1,18 +1,25 @@
-/* global location */
-
-import { installUi } from './jsxcad-ui-v1.js';
+/* global location, window */
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    const bootstrap = async () => {
+    const bootstrap = async (sha) => {
       const booting = document.createElement('center');
-      booting.innerText = `Booting sha`;
+      booting.innerText = sha;
       document.getElementById('loading').appendChild(booting);
+      const module = await import(`https://gitcdn.link/cdn/jsxcad/JSxCAD/${sha}/es6/jsxcad-ui-v1.js`);
       const hash = location.hash.substring(1);
       const [project, source] = hash.split('@');
-      await installUi({ document, project, source, sha: 'master' });
+      await module.installUi({
+        document,
+        project,
+        source
+        sha,
+      });
       document.body.removeChild(document.getElementById('loading'));
     };
-    bootstrap();
+
+    window.fetch('https://api.github.com/repos/jsxcad/JSxCAD/commits?per_page=1')
+          .then(response => response.json())
+          .then(json => bootstrap(json[0].sha));
   }
 };
